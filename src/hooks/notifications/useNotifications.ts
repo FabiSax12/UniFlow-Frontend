108b3e16f4800528c6b50a6d9292b5e6ce24b46e
Fabian Vargas <fabidev18@gmail.com>
2025-09-24 13:22:57 -0600

    feat: add delete notification functionality and enhance notification handling

--- src/hooks/notifications/useNotifications.ts
+++ src/hooks/notifications/useNotifications.ts
@@ -10,7 +10,7 @@ export function useNotifications(userId: string) {
       console.log(a)
       return a
     },
-    staleTime: 1000 * 60 * 5
+    // staleTime: 1000 * 60 * 5
   })
 
   return notificationsQuery;
--- src/hooks/notifications/useReadNotification.ts
+++ src/hooks/notifications/useReadNotification.ts
@@ -1,27 +1,38 @@
-import { markNotificationAsRead } from "@/api/notifications/endpoints/markNotificationAsRead";
 import type { Notification } from "@/domain/notifications";
 import { useAuthStore } from "@/stores/auth";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import type { MarkNotificationAsReadResponseDto } from "@/api/notifications";
+import { useOptimisticMutation } from "../useOptimisticMutation";
+import { markNotificationAsRead } from "@/api/notifications/endpoints/markNotificationAsRead";
 
 export function useReadNotification() {
 
-  const queryClient = useQueryClient();
-
   const { userInfo } = useAuthStore();
+  const authStore = useAuthStore()
+  console.log(authStore)
 
-  const markAsReadMutation = useMutation({
+  const markAsReadMutation = useOptimisticMutation<MarkNotificationAsReadResponseDto, { id: string }, Notification>({
     mutationKey: ["notifications", "mark-read"],
-    mutationFn: markNotificationAsRead,
-    onSuccess: (_, notificationId) => {
-
-
-      queryClient.setQueryData<Notification[]>(
-        ["notifications", userInfo.studentId],
-        (oldData) => {
-          return oldData?.map(notification => notification.id === notificationId ? notification.markAsRead() : notification)
-        }
-      )
-    }
+    invalidateOnSeattled: false,
+    mutationFn: ({ id }) => markNotificationAsRead(id),
+    queryKey: ["notifications", userInfo.id],
+    optimisticUpdate: (oldData, { id }) => oldData.map(n => {
+      console.log("Notificacion: ", n.id, "id readed", id)
+      if (n.id === id) return n.markAsRead();
+      return n;
+    }),
+    onMutateToast: ({ id }) => ({
+      message: `Actualizando Notificacion...`,
+      id: `update-Notificacion-${id}`,
+    }),
+    onSuccessToast: (_, { id }) => ({
+      message: `Notificacion actualizado correctamente`,
+      id: `update-Notificacion-${id}`,
+    }),
+    onErrorToast: (error, { id }) => ({
+      message: `Error al actualizar Notificacion`,
+      description: error.message,
+      id: `update-Notificacion-${id}`,
+    }),
   })
 
   return markAsReadMutation
