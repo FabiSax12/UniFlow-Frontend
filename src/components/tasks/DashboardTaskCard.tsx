97d05ee68ba0484fc991771f58486efe9dbf2606
Fabian Vargas <fabidev18@gmail.com>
2025-09-25 13:23:43 -0600

    fix: remove unused className prop from DashboardTaskCard component

--- src/components/tasks/DashboardTaskCard.tsx
+++ src/components/tasks/DashboardTaskCard.tsx
@@ -6,17 +6,17 @@ import { TaskPriorityBadge } from './TaskPriorityBadge'
 
 interface Props {
   task: TaskWithSubject
-  className?: string  
+  className?: string
 }
 
 const DashboardTaskCard = ({ task }: Props) => {
   return (
-       <Link to="/dashboard/tasks/$taskId" params={{ taskId: task.id }} className={`rounded-xl border-l-8 px-8 py-4 border flex hover:scale-x-105 transition-all duration-300 cursor-pointer hover:border-primary ${className}`}>
+    <Link to="/dashboard/tasks/$taskId" params={{ taskId: task.id }} className={`rounded-xl border-l-8 px-8 py-4 border flex hover:scale-x-105 transition-all duration-300 cursor-pointer hover:border-primary`}>
       {/* Left side */}
       <div className='flex-1'>
         <div className='mb-2 '>
-          <div className={`flex items-start justify-between mb-1 ${className}`}>
-            <h3 className='text-2xl ${className}'>
+          <div className={`flex items-start justify-between mb-1 `}>
+            <h3 className='text-2xl '>
               {task.title}
             </h3>
             <div className='flex gap-3'>
@@ -26,13 +26,13 @@ const DashboardTaskCard = ({ task }: Props) => {
               <TaskPriorityBadge priority={task.priority} />
             </div>
           </div>
-          <span className={`text-muted-foreground text-lg ${className}`}>
+          <span className={`text-muted-foreground text-lg `}>
             {task.subjectName} | {task.subjectCode}
           </span>
         </div>
         {/* <p className='text-muted-foreground mb-2 text-base'>{task.description}</p> */}
         <div className='flex justify-between'>
-          <span className={`flex text-base items-center gap-2 text-muted-foreground ${className}`}>
+          <span className={`flex text-base items-center gap-2 text-muted-foreground `}>
             <Calendar className='size-4' />
             {task.dueDate.toLocaleDateString('es-CR', { day: '2-digit', month: 'short' })}
             {" a las "}
