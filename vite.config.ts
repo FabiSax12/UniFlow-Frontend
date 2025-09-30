2b39159484029ffc55c992616af47476cd9ce401
Fabian Vargas <fabidev18@gmail.com>
2025-09-29 23:04:49 -0600

    feat: update HTML language attribute, add TanStack DevTools to Vite config, and update dependencies in package.json and lockfile

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import { resolve } from 'node:path'
+import { devtools } from '@tanstack/devtools-vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +12,9 @@ export default defineConfig({
     tanstackRouter({ autoCodeSplitting: true }),
     viteReact(),
     tailwindcss(),
+    devtools({
+      removeDevtoolsOnBuild: true
+    }),
   ],
   resolve: {
     alias: {
